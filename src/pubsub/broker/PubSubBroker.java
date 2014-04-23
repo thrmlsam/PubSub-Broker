ad5948970fc9af2f8ba78cbb04d69a82466a8091
Thirumalai Samy Rajkumar <thrmlsam@gmail.com>
2014-04-23 18:20:41 -0400

    Project created

---
+++ src/pubsub/broker/PubSubBroker.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pubsub.broker;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author thirumalaisamy
+ */
+public class PubSubBroker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try {
+            
+            int port;
+            if (args.length > 0) {
+                port = Integer.parseInt(args[0]);
+            } else {
+                port = 8080;
+            }
+            System.out.println( "Starting Server at port " + port );
+            new Server(port).run();
+        } catch (Exception ex) {
+            Logger.getLogger(PubSubBroker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ src/pubsub/broker/datahandler/ClientDataHandler.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pubsub.broker.datahandler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import pubsub.message.NetworkMessage.Messages;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ClientDataHandler extends SimpleChannelInboundHandler<Messages>{
+      private static final Logger logger = Logger.getLogger(
+            ClientDataHandler.class.getName());
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Connected to :" + ctx.channel().remoteAddress());
+    }
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.log(
+                Level.WARNING,
+                "Unexpected exception from downstream.", cause);
+        ctx.close();
+    }
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Messages msg) throws Exception {
+        
+    }
+    
+}
