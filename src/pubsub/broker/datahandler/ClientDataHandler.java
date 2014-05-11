4c03fa86967a2564671726df1f5136d623f4cb0b
alagurit <alaguvalliappan@gmail.com>
2014-05-11 03:10:44 -0400

    modified for subscribe/unsubscribe

--- src/pubsub/broker/datahandler/ClientDataHandler.java
+++ src/pubsub/broker/datahandler/ClientDataHandler.java
@@ -112,10 +112,57 @@ public class ClientDataHandler extends SimpleChannelInboundHandler<Messages> {
                 for(int i=0;i<allTopics.size();i++){
                     reply.addTopics(allTopics.get(i));
                 }
-                ctx.channel().writeAndFlush(reply.build());
-                
+                ctx.channel().writeAndFlush(reply.build());  
             }
-            
+            else if(msg.getMessageType() == Messages.MessageType.GET_SUBSCRIBEDTOPICS){
+                System.out.println("Broker:  GET_SUBSCRIBEDTOPICS");
+                Topics topics = new Topics();
+                ArrayList<String> subscribedTopics = null;
+                if(msg.getSubscriber().hasEmail()){
+                    System.out.println("sub has email");
+                    subscribedTopics = topics.getSubscribedTopicsEmail(msg.getSubscriber().getEmail());
+                }
+                else if(msg.getSubscriber().hasHostAddress()){
+                    System.out.println("sub has hostaddr");
+                    subscribedTopics = topics.getSubscribedTopicsIP(msg.getSubscriber().getHostAddress());
+                }
+                //ctx.channel().remoteAddress().toString()
+                
+                Messages.Builder reply = Messages.newBuilder();
+                reply.setMessageType(Messages.MessageType.GET_SUBSCRIBEDTOPICS);
+                for(int i=0;i<subscribedTopics.size();i++){
+                    reply.addTopics(subscribedTopics.get(i));
+                }
+                ctx.channel().writeAndFlush(reply.build());  
+            }
+            else if(msg.getMessageType() == Messages.MessageType.ADD_SUBSCRIBER){
+                System.out.println("Broker:  ADD_SUBSCRIBER");
+                Topics topics = new Topics();
+                
+                if(msg.getSubscriber().hasEmail()){
+                    System.out.println("sub has email");
+                    System.out.println(msg.getTopics(0) + "-" + msg.getSubscriber().getEmail());
+                    topics.addSubscriberByEmail(msg.getTopics(0), msg.getSubscriber().getEmail());
+                }
+                else if(msg.getSubscriber().hasHostAddress()){
+                    System.out.println("sub has hostaddr");
+                    topics.addSubscriberByHostAddress(msg.getTopics(0), msg.getSubscriber().getHostAddress());
+                }
+            }
+            else if(msg.getMessageType() == Messages.MessageType.REMOVE_SUBSCRIBER){
+                System.out.println("Broker:  REMOVE_SUBSCRIBER");
+                Topics topics = new Topics();
+                
+                if(msg.getSubscriber().hasEmail()){
+                    System.out.println("sub has email");
+                    System.out.println(msg.getTopics(0) + "-" + msg.getSubscriber().getEmail());
+                    topics.removeSubscriberEmail(msg.getTopics(0), msg.getSubscriber().getEmail());
+                }
+                else if(msg.getSubscriber().hasHostAddress()){
+                    System.out.println("sub has hostaddr");
+                    topics.removeSubscriberHostAddress(msg.getTopics(0), msg.getSubscriber().getHostAddress());
+                }
+            }
         } catch (Exception ex) {
             logger.log(
                     Level.SEVERE,
--- src/pubsub/broker/model/Topics.java
+++ src/pubsub/broker/model/Topics.java
@@ -6,10 +6,13 @@
 
 package pubsub.broker.model;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import com.mongodb.WriteResult;
 import java.util.ArrayList;
+import java.util.List;
 import net.karmafiles.ff.core.tool.dbutil.converter.Converter;
 import pubsub.broker.database.DBConstants;
 import pubsub.broker.database.DataAccess;
@@ -21,9 +24,10 @@ import pubsub.broker.database.IDataStore;
  */
 public class Topics extends DataAccess implements IDataStore{
 
+    public Topics(){}
     private String topic;
-    private ArrayList<String> email_list;
-    private ArrayList<String> host_list;
+    private List<String> email_list;
+    private List<String> host_list;
 
     public String getTopic() {
         return topic;
@@ -33,7 +37,7 @@ public class Topics extends DataAccess implements IDataStore{
         this.topic = topic;
     }
 
-    public ArrayList<String> getEmail_list() {
+    public List<String> getEmail_list() {
         return email_list;
     }
 
@@ -41,7 +45,7 @@ public class Topics extends DataAccess implements IDataStore{
         this.email_list = email_list;
     }
 
-    public ArrayList<String> getHost_list() {
+    public List<String> getHost_list() {
         return host_list;
     }
 
@@ -67,12 +71,223 @@ public class Topics extends DataAccess implements IDataStore{
 	while (cursor.hasNext()) {
             DBObject dbo = cursor.next();
             if(dbo!=null){
+                
                 Topics topics = Converter.toObject(Topics.class, dbo);
                 allTopics.add(topics.getTopic());
+                System.out.println("topic "+topics);
             }
         }
-        System.out.println(collection.count() + " - " + allTopics.size());
+        System.out.println("SubscribedTopicsAll: " + collection.count() + " - " + allTopics.size());
         return allTopics;
+    }
+
+    public ArrayList<String> getSubscribedTopicsEmail(String email) {
+        DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        ArrayList<String> subscribedTopics = new ArrayList<String>();
+        DBCursor cursor = collection.find();
+	while (cursor.hasNext()) {
+            //System.out.println("cursor");
+            
+            ArrayList<String> topicList = new ArrayList<>();
+            DBObject dbo = cursor.next();
+            if(dbo!=null){
+                //System.out.print("dbo");
+                System.out.println(dbo.get(DBConstants.TOPIC_EMAIL_LIST));
+                Topics topics = Converter.toObject(Topics.class, dbo);
+                //System.out.println(topics);
+                ArrayList<String> emailList = new ArrayList<>();
+                /*if(topics.getEmail_list()!=null)
+                emailList.addAll(topics.getEmail_list());*/
+                emailList.addAll((List<String>)dbo.get(DBConstants.TOPIC_EMAIL_LIST));
+                //topicList.add((String)dbo.get(DBConstants.TOPIC_TOPIC));
+                System.out.println("-"+emailList);
+                for(int i=0;i<emailList.size();i++){
+                    System.out.println(emailList.get(i));
+                    if(emailList.get(i).trim().equalsIgnoreCase(email.trim())){
+                       System.out.println("loop" + i);
+                       subscribedTopics.add(topics.getTopic());
+                       break;
+                    }
+                
+                if(emailList.contains(email)){
+                    subscribedTopics.add(topics.getTopic());
+                }
+                }
+            }
+        }
+        /*DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        ArrayList<String> subscribedTopics = new ArrayList<String>();
+        DBCursor cursor = collection.find();
+	while (cursor.hasNext()) {
+            System.out.println("email");
+            DBObject dbo = cursor.next();
+            if(dbo!=null){
+                Topics topics = Converter.toObject(Topics.class, dbo);
+                subscribedTopics.add(topics.getTopic());
+            }
+        }*/
+        System.out.println("SubscribedTopicsEmail: " + collection.count() + " - " + subscribedTopics.size());
+        return subscribedTopics;
+    }
+
+    public ArrayList<String> getSubscribedTopicsIP(String hostAddress) {
+        DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        ArrayList<String> subscribedTopics = new ArrayList<String>();
+        DBCursor cursor = collection.find();
+	while (cursor.hasNext()) {
+            //System.out.println("cursor");
+            
+            ArrayList<String> topicList = new ArrayList<>();
+            DBObject dbo = cursor.next();
+            if(dbo!=null){
+                //System.out.print("dbo");
+                System.out.println(dbo.get(DBConstants.TOPIC_HOST_LIST));
+                Topics topics = Converter.toObject(Topics.class, dbo);
+                //System.out.println(topics);
+                ArrayList<String> hostList = new ArrayList<>();
+                /*if(topics.getEmail_list()!=null)
+                emailList.addAll(topics.getEmail_list());*/
+                hostList.addAll((List<String>)dbo.get(DBConstants.TOPIC_HOST_LIST));
+                //topicList.add((String)dbo.get(DBConstants.TOPIC_TOPIC));
+                System.out.println("-"+hostList);
+                for(int i=0;i<hostList.size();i++){
+                    System.out.println(hostList.get(i));
+                    if(hostList.get(i).trim().equalsIgnoreCase(hostAddress.trim())){
+                       //System.out.println("loop" + i);
+                       subscribedTopics.add(topics.getTopic());
+                       break;
+                    }
+                
+                if(hostList.contains(hostAddress)){
+                    subscribedTopics.add(topics.getTopic());
+                }
+                }
+            }
+        }
+        
+        System.out.println("SubscribedTopicsEmail: " + collection.count() + " - " + subscribedTopics.size());
+        return subscribedTopics;
+        /*DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        ArrayList<String> subscribedTopics = new ArrayList<String>();
+        DBCursor cursor = collection.find();
+	while (cursor.hasNext()) {
+            DBObject dbo = cursor.next();
+            if(dbo!=null){
+                Topics topics = Converter.toObject(Topics.class, dbo);
+                //ArrayList<String> ipList = topics.getEmail_list();
+
+               // if(ipList.contains(hostAddress)){
+                 //   subscribedTopics.add(topics.getTopic());
+                //}
+            }
+        }
+        System.out.println("SubscribedTopicsEmail: " + collection.count() + " - " + subscribedTopics.size());
+        return subscribedTopics;*/
+    }
+    
+    @Override
+    public String toString(){
+        if(email_list != null)
+        return this.topic+" "+this.email_list.toString();
+        else return this.topic +" no list";
+    }
+
+    public void addSubscriberByHostAddress(String topics, String hostAddress) {
+        DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        //BasicDBObject query = new BasicDBObject(DBConstants.TOPIC_TOPIC, topics);
+        
+        BasicDBObject query = new BasicDBObject();
+        query.put(DBConstants.TOPIC_TOPIC, topics);
+        
+        DBObject dbo = collection.findOne(query);
+        
+        List<String> hostList = new ArrayList<String>();
+        hostList.addAll((List<String>)dbo.get(DBConstants.TOPIC_HOST_LIST));
+        hostList.add(hostAddress);
+
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put(DBConstants.TOPIC_HOST_LIST, hostList);
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", newDocument);
+
+        collection.update(query, updateObj);
+    }
+
+    public void addSubscriberByEmail(String topics, String email) {
+        DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        //BasicDBObject query = new BasicDBObject(DBConstants.TOPIC_TOPIC, topics);
+        
+        BasicDBObject query = new BasicDBObject();
+        query.put(DBConstants.TOPIC_TOPIC, topics);
+        
+        DBObject dbo = collection.findOne(query);
+        
+        List<String> emailList = new ArrayList<String>();
+        emailList.addAll((List<String>)dbo.get(DBConstants.TOPIC_EMAIL_LIST));
+        emailList.add(email);
+        
+        /*List<String> hostList = new ArrayList<String>();
+        hostList.addAll((List<String>)dbo.get(DBConstants.TOPIC_HOST_LIST));*/
+        
+        
+
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put(DBConstants.TOPIC_EMAIL_LIST, emailList);
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", newDocument);
+
+        collection.update(query, updateObj);
+        
+        /*BasicDBObject updateObj = new BasicDBObject(DBConstants.TOPIC_EMAIL_LIST, emailList);
+        updateObj.(DBConstants.TOPIC_EMAIL_LIST, emailList);
+        
+        WriteResult wr = collection.update(query, updateObj);*/
+        
+    }
+
+    public void removeSubscriberEmail(String topics, String email) {
+        DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        //BasicDBObject query = new BasicDBObject(DBConstants.TOPIC_TOPIC, topics);
+        
+        BasicDBObject query = new BasicDBObject();
+        query.put(DBConstants.TOPIC_TOPIC, topics);
+        
+        DBObject dbo = collection.findOne(query);
+        
+        List<String> emailList = new ArrayList<String>();
+        emailList.addAll((List<String>)dbo.get(DBConstants.TOPIC_EMAIL_LIST));
+        emailList.remove(email);
+        
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put(DBConstants.TOPIC_EMAIL_LIST, emailList);
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", newDocument);
+
+        collection.update(query, updateObj);
+    }
+
+    public void removeSubscriberHostAddress(String topics, String hostAddress) {
+        DBCollection collection = db.getCollection(DBConstants.TOPIC_COLLECTION);
+        
+        BasicDBObject query = new BasicDBObject();
+        query.put(DBConstants.TOPIC_TOPIC, topics);
+        
+        DBObject dbo = collection.findOne(query);
+        
+        List<String> hostList = new ArrayList<String>();
+        hostList.addAll((List<String>)dbo.get(DBConstants.TOPIC_HOST_LIST));
+        hostList.remove(hostAddress);
+        
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put(DBConstants.TOPIC_HOST_LIST, hostList);
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", newDocument);
+
+        collection.update(query, updateObj);
     }
 }
 
